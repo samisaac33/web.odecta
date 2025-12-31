4fe56e0910fec7b3d6ad0b86fdbd3fa6db3f8bbe
samisaac33 <contacto@paredimia.com>
2025-12-31 11:44:26 -0500

    Update SummarySlide.tsx

--- components/slides/SummarySlide.tsx
+++ components/slides/SummarySlide.tsx
@@ -17,7 +17,8 @@ const SummarySlide: React.FC<SlideProps> = () => {
             src="https://drive.google.com/thumbnail?id=1rls9bpUrVQiIF8GmSdPowtUqXZqVlrH3&sz=w2000" 
             alt="Visualización Estratégica Macrolote Odecta" 
             className="w-full h-auto object-cover"
-        </p>
+          />
+        </div>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 flex-grow">
